b8edf4f6a6318337b375ac8c7fd0d4f7be759592
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2024-03-29 19:54:57 +0100

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './App.css';
 
 const artists = ["mos def", "jurassic 5", "beastie boys", "artifacts", "dr octagon", "a tribe called quest"];
 
@@ -36,7 +37,7 @@ export default function App() {
         const albums = json.topalbums.album.filter((el, idx) => idx < 2);
         albums.forEach(album => {
           const obj = {};
-          const albumImage = album.image.find(el => el.size === "large")["#text"];
+          const albumImage = album.image.find(el => el.size === "extralarge")["#text"];
           obj.albumName = album.name;
           obj.albumImage = albumImage;
           obj.artist = artistName;
@@ -49,20 +50,21 @@ export default function App() {
   }, [isDataFetched]);
 
   return (
-    <div>
+    <div className="albums">
       {
         isDataFetched &&
         (
           data.map(album =>
-            <div key={album.artist + album.albumName}>
-              <div>
+            <div className="album" key={album.artist + album.albumName}>
+              <div className="albumName">
+                <h1> {album.albumName} </h1>
+              </div>
+              <div className="albumArtist">
+                <h1> {album.artist} </h1>
+              </div>
+              <div className="albumImage">
                 <img src={album.albumImage} alt="" />
               </div>
-              <div>
-                <p>{album.albumName}</p>
-                <p>{album.artist}</p>
-              </div>
-
             </div>
           )
         )
@@ -70,43 +72,3 @@ export default function App() {
     </div>
   );
 }
-
-
-/*
-
-  // http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=YOUR_API_KEY&format=json
-  // /2.0/?method=artist.gettopalbums&artist=cher&api_key=YOUR_API_KEY&format=json 
-  useEffect(() => {
-    if (typeof data === "object") return;
-    const baseURL = `http://ws.audioscrobbler.com/2.0/`;
-    const apiKey = `ce5e7f34b1972343c0feaf5829cbe842`;
-
-    let albumsArr = [];
-    for (let i = 0; i < artists.length; i++) {
-      let URL = `${baseURL}?method=artist.gettopalbums&artist=${artists[i]}&api_key=${apiKey}&format=json`;
-      fetchData(URL).then(json => {
-        const obj = {};
-        obj.artist = json.topalbums["@attr"].artist;
-        obj.albums = json.topalbums.album.filter((el, idx) => idx < 2)
-          .map(album => {
-            const albumImage = album.image.find(el => el.size === "large")["#text"];
-            return { name: album.name, image: albumImage }
-          });
-        albumsArr.push(obj);
-        console.log(obj);
-      });
-    }
-
-    async function fetchData(url) {
-      const response = await fetch(url);
-      const json = await response.json();
-      return json;
-    }
-
-    setData(albumsArr);
-    localStorage.setItem("albums", JSON.stringify(albumsArr));
-
-  }, [data]);
-
-
-  */
